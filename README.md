503376867326c9d59177215f6b94718d9de635f4
Jason Bedard <jason+github@jbedard.ca>
2019-11-22 13:48:46 -0800

    feat(*): initial implementation based on https://github.com/bazelbuild/rules_nodejs/tree/0.41.0/packages/inject-html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # html-insert-assets
 Insert assets such as .js, .css into an HTML file.
+
+Originally forked from https://github.com/bazelbuild/rules_nodejs/tree/0.41.0/packages/inject-html.
---
+++ test/spec.js
@@ -0,0 +1,73 @@
+const inserter = require('../src/main');
+
+describe('HTML inserter', () => {
+  const inFile = 'data/some/index.html';
+  const outFile = 'out/some/index.html';
+
+  let output;
+  function read(file) {
+    if (file === inFile) return `<html><head></head><body></body></html>`;
+    throw new Error(`no content for ${file}`);
+  }
+  function write(_, content) {
+    output = content;
+  }
+
+  it('should do be a no-op', () => {
+    expect(inserter.main([outFile, inFile], read, write)).toBe(0);
+    expect(output).toBe('<html><head></head><body></body></html>');
+  });
+
+  it('should inject script tag', () => {
+    expect(inserter.main([outFile, inFile, '--assets', 'path/to/my.js'], read, write, () => 123)).toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script src="/path/to/my.js?v=123"></script></body></html>');
+  });
+
+  it('should allow the "module js" extension', () => {
+    expect(inserter.main([outFile, inFile, '--assets', 'path/to/my.mjs'], read, write, () => 123))
+        .toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script type="module" src="/path/to/my.mjs?v=123"></script></body></html>');
+  });
+
+  it('should allow the ".es2015.js" extension', () => {
+    expect(inserter.main(
+               [outFile, inFile, '--assets', 'path/to/my.es2015.js'], read, write, () => 123))
+        .toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script type="module" src="/path/to/my.es2015.js?v=123"></script></body></html>');
+  });
+
+  it('should strip longest prefix', () => {
+    expect(inserter.main([outFile, inFile, 
+      'path', 'path/to',
+      '--assets', 'path/to/my.js'], read, write, () => 123)).toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script src="/my.js?v=123"></script></body></html>');
+  });
+
+  it('should strip external workspaces', () => {
+    expect(inserter.main([outFile, inFile, 
+      'npm/node_modules/zone.js/dist',
+      '--assets', 'external/npm/node_modules/zone.js/dist/zone.min.js'], read, write, () => 123)).toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script src="/zone.min.js?v=123"></script></body></html>');
+    
+  });
+
+  it('should inject link tag', () => {
+    expect(inserter.main([outFile, inFile, '--assets', 'path/to/my.css'], read, write, () => 123)).toBe(0);
+    expect(output).toBe(
+        '<html><head><link rel="stylesheet" href="/path/to/my.css?v=123"></head><body></body></html>');
+  });
+
+  it('should create a pair of script tags for differential loading', () => {
+    expect(inserter.main(
+               [outFile, inFile, '--assets', 'path/to/my.js', 'path/to/my.es2015.js'], read, write,
+               () => 123))
+        .toBe(0);
+    expect(output).toBe(
+        '<html><head></head><body><script nomodule="" src="/path/to/my.js?v=123"></script><script type="module" src="/path/to/my.es2015.js?v=123"></script></body></html>');
+  });
+});
